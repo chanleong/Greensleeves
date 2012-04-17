74bb5be6aafa06239dfe78eaa235969a616f4bc7
Raymond <am5a03@gmail.com>
2012-04-17 10:43:58 +0800

    TFNG = True false not given

--- Greensleeves/src/Main.java
+++ Greensleeves/src/Main.java
@@ -24,7 +24,8 @@ public class Main {
 		Essay e = new Essay("lib/test2.txt");
 		TFNG tfng = new TFNG();
 		
-		tfng.questionGen(e);
+		//System.out.println(e.getParagraph(1).getSentenceStr(0));
+		tfng.questionGen(e.getParagraph(0).getSentence(0));
 		
 //		String discoDir = "C:\\Documents and Settings\\Raymond\\git\\Greensleeves\\Greensleeves\\lib\\wordbase";
 //		DISCO disco = new DISCO(discoDir, false);
@@ -47,50 +48,50 @@ public class Main {
 //			//r.getRankedSentences(e.getParagraph(i));
 //		}
 		
-		System.out.println("Find specific fact, e.g. LOCATION");
-		ArrayList<String> temp = FactEvaluator.getFact(FactEvaluator.type.LOCATION, e.getEssayStr());
-		display(temp);
+//		System.out.println("Find specific fact, e.g. LOCATION");
+//		ArrayList<String> temp = FactEvaluator.getFact(FactEvaluator.type.LOCATION, e.getEssayStr());
+//		display(temp);
+//		
+//		System.out.println("Find all fact, the 7 types");
+//		temp = FactEvaluator.getAllUniqueFacts(e.toString());
+//		display(temp);
+//		
+//		System.out.println("Evaluate the key word:");
+//		String str = FactEvaluator.getKeyFact(e.getEssayStr());
+//		System.out.println(str);
+//		System.out.println();
+//		
+//		System.out.println("Show sentence that consist of a fact in the whole essay");
+//		ArrayList<Sentence> temp2 = FactEvaluator.getSentenceAbout("Sirius", e);
+//		for(int i = 0; i < temp2.size(); i++)
+//		{
+//			System.out.println(temp2.get(i).getSentence());
+//		}
 		
-		System.out.println("Find all fact, the 7 types");
-		temp = FactEvaluator.getAllUniqueFacts(e.toString());
-		display(temp);
 		
-		System.out.println("Evaluate the key word:");
-		String str = FactEvaluator.getKeyFact(e.getEssayStr());
-		System.out.println(str);
-		System.out.println();
+		MCQ mcq = new MCQ();
+		mcq.questionGen(e.getParagraph(1));
 		
-		System.out.println("Show sentence that consist of a fact in the whole essay");
-		ArrayList<Sentence> temp2 = FactEvaluator.getSentenceAbout("Sirius", e);
-		for(int i = 0; i < temp2.size(); i++)
-		{
-			System.out.println(temp2.get(i).getSentence());
+		System.out.println(mcq.getAnsPair().getLeft());
+		for(int i = 0; i < mcq.getAnsPair().getRight().length; i++){
+			System.out.println(mcq.getAnsPair().getRight()[i]);
 		}
+		System.out.println(mcq.getAns());
 		
 		
-//		MCQ mcq = new MCQ();
-//		mcq.questionGen(e.getParagraph(1));
-//		
-//		System.out.println(mcq.getAnsPair().getLeft());
-//		for(int i = 0; i < mcq.getAnsPair().getRight().length; i++){
-//			System.out.println(mcq.getAnsPair().getRight()[i]);
-//		}
-//		System.out.println(mcq.getAns());
+		ParagraphHeading ph = new ParagraphHeading();		
+		ph.questionGen(e);		
+		InfoIdentification ii = new InfoIdentification(1, 8);	
+		//ii.testEssay = e;
+		System.out.println(ii.getQuestion());
+		System.out.println(ii.getInstructions()[0]);
+		ii.questionGen(e);
 		
+		ArrayList<Pair<Integer, String>> qa = ii.getQAset();
 		
-//		ParagraphHeading ph = new ParagraphHeading();		
-//		ph.questionGen(e);		
-//		InfoIdentification ii = new InfoIdentification(1, 8);	
-//		//ii.testEssay = e;
-//		System.out.println(ii.getQuestion());
-//		System.out.println(ii.getInstructions()[0]);
-//		ii.questionGen(e);
-//		
-//		ArrayList<Pair<Integer, String>> qa = ii.getQAset();
-//		
-//		for(int i = 0; i < qa.size(); i++){
-//			System.out.println(qa.get(i));
-//		}
+		for(int i = 0; i < qa.size(); i++){
+			System.out.println(qa.get(i));
+		}
 		
 //		String[] jj = ii.getQuestionSet();
 //		
